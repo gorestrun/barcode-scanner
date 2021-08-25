d9a098070a4f57cab4d8c302f6f44a9aae222843
gorestrun <5555867+gorestrun@users.noreply.github.com>
2021-08-25 18:17:16 +0800

    Force the launched camera to point to the back camera, by default

--- src/main/resources/static/js/setup-barcode-scanner.js
+++ src/main/resources/static/js/setup-barcode-scanner.js
@@ -14,7 +14,7 @@ fetch(url)
 	 })
 	.catch((err) => console.log("Can’t access " + url + " response. Error: " + err));
 
-window.addEventListener('load', function(){    
+window.addEventListener('load', function(){
     let selectedDeviceId;
     const barcodeReader = new ZXing.BrowserBarcodeReader();
     console.log('Window loaded, barcode reader initialized');
@@ -22,36 +22,51 @@ window.addEventListener('load', function(){
         .then((videoInputDevices) => {
             const sourceSelect = document.getElementById('barcodeScannerSourceSelect');
             selectedDeviceId = videoInputDevices[0].deviceId;
-    
+
             if (videoInputDevices.length > 1) {
+                const sourceOption = document.createElement('option');
+                sourceOption.text = "Select an option:";
+                sourceOption.value = "";
+                sourceOption.selected = true;
+                sourceOption.disabled = true;
+                sourceSelect.appendChild(sourceOption);
+
                 videoInputDevices.forEach((element) => {
                     const sourceOption = document.createElement('option');
                     sourceOption.text = element.label;
                     sourceOption.value = element.deviceId;
-                    
+
                     sourceSelect.appendChild(sourceOption);
                 })
 				
 				sourceSelect.onchange = () => {
                 	selectedDeviceId = sourceSelect.value;
+                    if(selectedDeviceId != ""){
+                        decode(barcodeReader, selectedDeviceId);
+                    }
             	}	        
             
                 const sourceSelectPanel = document.getElementById('barcodeScannerSourceSelectPanel');
                 sourceSelectPanel.style.display = 'block';
             }
-            
+
             document.getElementById('barcodeScannerBtn').addEventListener('click', () => {
-					barcodeReader.decodeOnceFromVideoDevice(selectedDeviceId, 'video').then((result) => {
-			        console.log(result);
-			        document.getElementById('barcode').textContent = result.text;
-			    }).catch((err) => {
-			        console.error(err);
-			        document.getElementById('barcode').textContent = ''; //When the user change camera source, an error will be displayed. We do not want this.
-			    })
-			    console.log(`Started continuous decode from camera with id ${selectedDeviceId}`)
+                decode(barcodeReader, undefined); //undefined will force the camera to point to the back camera(environment facing)
 			})
         })
         .catch((err) => {
             console.error(err);
         })
 })
+
+function decode(barcodeReader, selectedDeviceId){
+
+	barcodeReader.decodeOnceFromVideoDevice(selectedDeviceId, 'video').then((result) => {
+        console.log("Decoded value:" + result);
+        document.getElementById('barcode').textContent = result.text;
+    }).catch((err) => {
+        console.error(err);
+        document.getElementById('barcode').textContent = ''; //When the user change camera source, an error will be displayed. We do not want this.
+    })
+    console.log(`Started continuous decode from camera with id ${selectedDeviceId}`)
+}
